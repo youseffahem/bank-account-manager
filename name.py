6b87d9437ebef98d86167e3ae11b07b57b7d3ea0
Youssef Fahem Amin <yousef.fahem11@gmail.com>
2025-07-19 02:35:31 +0300

    Remove unnecessary files and cleanup

--- name.py
+++
@@ -1,2 +0,0 @@
-def fname(fname):
-    return f"Hello {fname}"
--- test.py
+++
@@ -1,9 +0,0 @@
-class Std:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-
-x= str(input("ENter name: "))
-y=int(input("Enter age: "))
-std=Std(x,y)
-print(f"the student {std.name} his age = {std.age}")
